c4a0892cfc50c2f00f74adcb971c72f595a4c035
B0ttle-Cat <kolazkr.lgb@gmail.com>
2026-01-01 11:50:50 +0900

    202601011150

---
+++ Assets/Scripts/GameManager/UIManager/Component/PoolingContainer/PoolingDataContainer.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+using Sirenix.OdinInspector;
+
+using UnityEngine;
+
+namespace GameUI
+{
+    public class PoolingDataContainer : MonoBehaviour
+	{
+		[SerializeField, ReadOnly, Required]
+		protected PanelGroupComponent targetComponent; 
+		protected List<IObjectForPanel> dataList = new List<IObjectForPanel>();
+
+		protected virtual void OnValidate()
+		{
+			targetComponent = GetComponent<CardGroupPanelComponent>();
+		}
+
+		protected virtual void Awake()
+		{
+			targetComponent = GetComponent<CardGroupPanelComponent>();
+		}
+
+		public virtual void InitData(IEnumerable<IObjectForPanel> elements)
+		{
+			dataList.Clear();
+			AddData(elements);
+		}
+		public virtual void ClearData() { dataList.Clear(); }
+		public virtual void AddData(IObjectForPanel item)
+		{
+			dataList.Add(item);
+			targetComponent.AddItem(item);
+		}
+		public virtual void RemoveData(IObjectForPanel item)
+		{
+			dataList.Remove(item);
+			targetComponent.RemoveItem(item);
+		}
+		public virtual void AddData(IEnumerable<IObjectForPanel> elements)
+		{
+			foreach (var item in elements)
+			{
+				dataList.Add(item);
+				targetComponent.AddItem(item);
+			}
+		}
+		public virtual void RemoveData(IEnumerable<IObjectForPanel> elements)
+		{
+			foreach (var item in elements)
+			{
+				dataList.Remove(item);
+				targetComponent.RemoveItem(item);
+			}
+		}
+	}
+}
--- Assets/Scripts/GameManager/UIManager/Component/PoolingContainer/ScrollPoolingDataContainer.cs
+++ Assets/Scripts/GameManager/UIManager/Component/PoolingContainer/ScrollPoolingDataContainer.cs
@@ -7,57 +7,7 @@ using UnityEngine.UI;
 
 namespace GameUI
 {
-
-	public class PoolingDataContainer : MonoBehaviour
-	{
-		[SerializeField, ReadOnly, Required]
-		protected PanelGroupComponent targetComponent; 
-		protected List<IObjectForPanel> dataList = new List<IObjectForPanel>();
-
-		protected virtual void OnValidate()
-		{
-			targetComponent = GetComponent<CardGroupPanelComponent>();
-		}
-
-		protected virtual void Awake()
-		{
-			targetComponent = GetComponent<CardGroupPanelComponent>();
-		}
-
-		public virtual void InitData(IEnumerable<IObjectForPanel> elements)
-		{
-			dataList.Clear();
-			AddData(elements);
-		}
-		public virtual void ClearData() { dataList.Clear(); }
-		public virtual void AddData(IObjectForPanel item)
-		{
-			dataList.Add(item);
-			targetComponent.AddItem(item);
-		}
-		public virtual void RemoveData(IObjectForPanel item)
-		{
-			dataList.Remove(item);
-			targetComponent.RemoveItem(item);
-		}
-		public virtual void AddData(IEnumerable<IObjectForPanel> elements)
-		{
-			foreach (var item in elements)
-			{
-				dataList.Add(item);
-				targetComponent.AddItem(item);
-			}
-		}
-		public virtual void RemoveData(IEnumerable<IObjectForPanel> elements)
-		{
-			foreach (var item in elements)
-			{
-				dataList.Remove(item);
-				targetComponent.RemoveItem(item);
-			}
-		}
-	}
-	public class PoolingScrollDataContainer : PoolingDataContainer
+	public class ScrollPoolingDataContainer : PoolingDataContainer
 	{
 		[SerializeField]
 		private RectTransform poolItemRect;
